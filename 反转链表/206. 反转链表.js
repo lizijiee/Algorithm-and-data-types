8a64cb581daaf71c8c46b051a8e508192c445b7a
lizijiee <562949697@qq.com>
2020-05-23 23:55:54 +0800

    尝试反转

--- 反转链表/206. 反转链表.js
+++ 反转链表/206. 反转链表.js
@@ -1,1 +1,45 @@
-console.log(222);
+const list = {
+    val: 1,
+    next: {
+        val: 2,
+        next: {
+            val: 3,
+            next: {
+                val: 4,
+                next: {
+                    val: 5,
+                    next: null
+                }
+            }
+        }
+    },
+};
+let newList = {};
+let reverseList = function (head) {
+    let newList = head; // 克隆整个树
+    let i = 0;
+
+    function ListNode(oldList) {
+        /* 
+            此处用于递归;
+            上一个节点prev,下一个阶段next;
+        */
+        prev = oldList.val;
+        // console.log("oldList:", oldList, head)
+        if (oldList.next) {
+            next = oldList.next;
+            oldList.val = oldList.next.val; // 下一个节点给上一个节点
+            oldList.next.val = prev;
+            ListNode(next);
+            return oldList
+        }
+        console.log("检查是否跳出循环！！！！！")
+    }
+
+    while (i < 4) {
+        console.log(i)
+        ListNode(newList);
+    }
+    return newList
+};
+console.log(reverseList(list))
